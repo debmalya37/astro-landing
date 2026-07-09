13e5c7efcd82d1a3e19cf2f210cd84a7dd399d72
Debmalya Sen <debmalyasen37@gmail.com>
2026-07-09 12:50:36 +0530

    feat: Adjust position of floating book image for improved visibility

--- app/page.tsx
+++ app/page.tsx
@@ -501,13 +501,12 @@ const time = useCountdown();
         </div>
 
         {/* 2. FLOATING SMALL BOOK (report-book.png) - Anchored perfectly to the Bottom-Left */}
-        <div className="absolute -bottom-6 -left-12 sm:-bottom-8 sm:-left-16 lg:-bottom-12 lg:-left-24 z-20 animate-float transition-all duration-700 w-[160px] sm:w-[220px] lg:w-[280px] aspect-[1/1.4] drop-shadow-[25px_30px_50px_rgba(26,15,10,0.5)] pointer-events-none">
+        <div className="absolute top-96 -left-12 sm:-bottom-8 sm:-left-16 lg:-bottom-12 lg:-left-24 z-20 animate-float transition-all duration-700 w-[160px] sm:w-[220px] lg:w-[280px] aspect-[1/1.4] drop-shadow-[25px_30px_50px_rgba(26,15,10,0.5)] pointer-events-none">
           <Image
             src="/report-book.png"
             alt="Floating Premium Kundali Report"
             width={280}
             height={392}
-            
             className="w-full h-full object-contain"
           />
         </div>
